a2ab7b95b336132e3bed66011d45c42d333144f8
omnis <aomnisz@gmail.com>
2020-01-10 19:19:09 +0800

    Update class ProofData
    make dataproof and parityproof public

--- src/main/java/com/fchen_group/AuditingwithErrorCorrection/main/ProofData.java
+++ src/main/java/com/fchen_group/AuditingwithErrorCorrection/main/ProofData.java
@@ -1,9 +1,9 @@
 package com.fchen_group.AuditingwithErrorCorrection.main;
 
 public class ProofData {
-	byte[] dataproof;
-	byte[] parityproof;
-	
+	public byte[] dataproof;
+	public byte[] parityproof;
+
 	public ProofData (byte[] dataproof,byte[] parityproof) {
 		this.dataproof=dataproof;
 		this.parityproof=parityproof;
