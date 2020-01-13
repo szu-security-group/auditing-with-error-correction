3c0a657f6292b6166aa6911c1b6d111bd1354865
omnis <aomnisz@gmail.com>
2020-01-13 20:09:09 +0800

    Add CoolProtocol.java

---
+++ src/main/java/com/fchen_group/AuditingwithErrorCorrection/Run/CoolProtocol.java
@@ -0,0 +1,43 @@
+package com.fchen_group.AuditingwithErrorCorrection.Run;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class CoolProtocol {
+    public static int magicNumber = 329;
+
+    public int filenameLength;
+    public int contentLength;
+    public byte[] filename;
+    public byte[] content;
+
+    public CoolProtocol(byte[] filename, byte[] content) {
+        this.filenameLength = filename.length;
+        this.contentLength = content.length;
+
+        try {
+            File file = new File(new String(filename));
+            FileInputStream fileInputStream = new FileInputStream(file);
+            this.content = new byte[(int) file.length()];
+            this.contentLength = fileInputStream.read(this.content);
+            fileInputStream.close();
+        } catch (FileNotFoundException e) {
+            this.contentLength = content.length;
+            this.content = content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "SmartCarProtocol{" +
+                "filenameLength=" + filenameLength +
+                ", contentLength=" + contentLength +
+                ", filename=" + new String(filename) +
+                ", content=" + new String(content) +
+                '}';
+    }
+}
