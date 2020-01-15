d5b3927d452f4307d858f3d3db328e7b5fa8bd84
omnis <aomnisz@gmail.com>
2020-01-15 15:54:34 +0800

    Server 和 Client: 增加序列化到数组的功能

--- src/main/java/com/fchen_group/AuditingwithErrorCorrection/Run/Client.java
+++ src/main/java/com/fchen_group/AuditingwithErrorCorrection/Run/Client.java
@@ -277,6 +277,19 @@ public class Client {
         }
     }
 
+    public static byte[] serialize(Object object) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(object);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        return objectInputStream.readObject();
+    }
+
     public static void print(byte[] data) {
         for (int i = 0; i < 10; i++) {
             System.out.print(String.format("%02x ", data[i]));
