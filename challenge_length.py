119a276533d66038f41ce4e4251d4c6660472944
Mitmeng <37100342+Mitmeng@users.noreply.github.com>
2020-04-17 20:52:41 +0800

    Add files via upload

---
+++ challenge_length.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from functools import reduce
+
+def count_challenge(n):    
+    count = int(4 * n ** 0.5)       #upper bound for challenge lentgh
+    blocks_broken = int (n * 0.01)
+    temp = [(n - blocks_broken - i) / n for i in range(count)]
+    for i in range(count):
+        prob_fail = reduce(lambda x, y: x * y, temp[0:i+1])
+        if prob_fail <= 0.01:
+            return [i, 1 - prob_fail]
+
+block_size = 1024
+n = [1 * (2 ** 30) / block_size,
+     5 * (2 ** 30) / block_size,
+     10 * (2 ** 30) / block_size,
+     15 * (2 ** 30) / block_size,
+     20 * (2 ** 30) / block_size,
+     40 * (2 ** 30) / block_size]
+print ([count_challenge(i) for i in n])
